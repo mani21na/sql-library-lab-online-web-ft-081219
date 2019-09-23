8a005e6545f01e7cf3426e3e0d4fa4323b720a4d
Hanna Lee <mani21na@gmail.com>
2019-09-23 00:12:46 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,11 +1,16 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT title, year FROM books WHERE series_id=1 ORDER BY year"
+  "SELECT title, year 
+  FROM books 
+  WHERE series_id=1 
+  ORDER BY year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT name, motto FROM characters ORDER BY LEGNTH(motto) DESC LIMIT 1;"
+  "SELECT name, motto 
+  FROM characters 
+  ORDER BY LEGNTH(motto) DESC 
+  LIMIT 1;"
 end
-
 
 def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT(species) 
