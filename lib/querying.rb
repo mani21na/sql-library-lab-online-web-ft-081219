c5a8fbcdbc6523475ba93cb895bdb9cb25743bdf
Hanna Lee <mani21na@gmail.com>
2019-09-23 00:49:01 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -30,13 +30,13 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT Series.title
-  FROM characters
-  INNER JOIN series
-  ON characters.series_id = series.id
-  GROUP BY Series.title, species
-  HAVING characters.species = 'human'
-  ORDER BY COUNT(species) DESC
+  "SELECT title 
+  FROM series
+  INNER JOIN characters 
+  ON series.author_id = characters.author_id
+  WHERE characters.species = 'human'
+  GROUP BY series.title
+  ORDER BY COUNT(characters.species) DESC 
   LIMIT 1;"
 end
 
